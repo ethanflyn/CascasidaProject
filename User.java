69319ae0b8f267fb5bb164206e9a5ed6e144b3c1
ethanflyn <102313981+ethanflyn@users.noreply.github.com>
2023-02-21 09:32:57 +0000

    Create User.java

---
+++ User.java
@@ -0,0 +1,9 @@
+public class User {
+    protected String name;
+    protected Tile [] tiles;
+
+    public User(String name, Tile [] tiles) {
+        this.name = name;
+        this.tiles = tiles;
+    }
+}
