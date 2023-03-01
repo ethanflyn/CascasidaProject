5d62b734bbe56a85cafa7f33e4b4a9100e46660a
daramacken <102314081+daramacken@users.noreply.github.com>
2023-03-01 14:29:18 +0000

    Update User.java

--- User.java
+++ User.java
@@ -1,9 +1,27 @@
-public class User {
-    protected String name;
-    protected Tile [] tiles;
+import java.util.ArrayList;
 
-    public User(String name, Tile [] tiles) {
-        this.name = name;
-        this.tiles = tiles;
+public class User {
+        protected String name;
+        protected Tile [] tiles;
+
+        protected Board myBoard = new Board();
+
+    public String getName() {
+        return name;
+    }
+
+    protected ArrayList<ArrayList<String>> allTiles;
+
+        public User(String name, Tile [] tiles) {
+            this.name = name;
+            this.tiles = tiles;
+        }
+
+    public Board getBoard() {
+        return myBoard;
+    }
+    public ArrayList<ArrayList<String>> getAllTiles(){
+            return allTiles;
     }
 }
+
