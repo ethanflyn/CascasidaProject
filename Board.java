d6b52e213bb9aadc65fa581c1f42982c1a2e0475
daramacken <102314081+daramacken@users.noreply.github.com>
2023-03-01 14:25:36 +0000

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,104 +1,118 @@
-import org.w3c.dom.html.HTMLTitleElement;
 
-import java.awt.*;
-import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 
 public class Board {
-    public static final int MAX_BOARD = 5;
 
-    private Tile[][] board = new Tile[MAX_BOARD][MAX_BOARD];
-    private int[][] intBoard = new int[MAX_BOARD][MAX_BOARD];
-    private int xCord;
-    private int yCord;
 
-    public void generateBoard() {
+    private static int counter = 0;
+
+    ArrayList<ArrayList<String>> myTiles = new ArrayList<>();
+
+    public Board() {
 
     }
-
 
 
     public void placeTile() {
 
+        System.out.println("\t\t\t\t\t\t\tTILE PLACEMENT\n\t\t\t\t\t\t\tPlease enter the X cordinate of your desired tile\n");
+        Scanner a = new Scanner(System.in);
+        String input = a.nextLine();
+        int x = Integer.parseInt(input);
+        if (x < 0 || x > 9) {
+            System.out.println("\t\t\t\t\t\t\tInvalid input!!!!");
+            placeTile();
+        }
+        System.out.println("\t\t\t\t\t\t\tPlease enter the Y cordinate of your desired tile");
+        Scanner b = new Scanner(System.in);
+        String input2 = b.nextLine();
+        int y = Integer.parseInt(input2);
+        if (y < 0 || y > 9) {
+            System.out.println("\t\t\t\t\t\t\tInvalid input!!!!");
+            placeTile();
+        }
+
+        input += input2;
+        int position = Integer.parseInt(input);
+
+        if (this.myTiles.get(position + 1) == Tile.EmptyTile && this.myTiles.get(position - 1) == Tile.EmptyTile && this.myTiles.get(position + 10) == Tile.EmptyTile && this.myTiles.get(position - 10) == Tile.EmptyTile) {
+            System.out.println("\t\t\t\t\t\t\tThere is no adjacent Tiless!!!!");
+            placeTile();
+        }
+        if (this.myTiles.get(position) != Tile.EmptyTile) {
+            System.out.println("\t\t\t\t\t\t\tThere is already a tile placed here!!!!");
+            placeTile();
+        }
+
+        this.myTiles.set(position, Tile.generateTile().TileDisplay());
+
+        showBoard(0);
+        System.out.println("\n\n");
+        GameIntro.NextTurn();
     }
 
     public void placeStarterTile() {
+        Tile a = Tile.generateTile();
+        Tile b = Tile.generateTile();
+        Tile c = Tile.generateTile();
+
+        this.myTiles.set(45, a.TileDisplay());
+        this.myTiles.set(55, b.TileDisplay());
+        this.myTiles.set(56, c.TileDisplay());
 
     }
 
 
     public static void main(String[] args) {
 
-        Board bo = new Board();
+    }
 
-        Tile a = Tile.generateTile();
-        Tile b = Tile.generateTile();
-        Tile c = Tile.generateTile();
-        Tile d = Tile.generateTile();
-        Tile e = Tile.generateTile();
-        Tile f = Tile.generateTile();
-        Tile g = Tile.generateTile();
-        Tile h = Tile.generateTile();
-        Tile i = Tile.generateTile();
-        Tile j = Tile.generateTile();
-        Tile k = Tile.generateTile();
-        Tile l = Tile.generateTile();
-        i.TileDisplay(); j.TileDisplay(); k.TileDisplay();l.TileDisplay();
-        a.TileDisplay(); b.TileDisplay(); c.TileDisplay(); d.TileDisplay(); e.TileDisplay(); f.TileDisplay(); g.TileDisplay(); h.TileDisplay();
+    public void fillBoard() {
 
-        bo.showBoard(1);
+
+        Tile.EmptyTile.add(TileColours.EMPTY_TILE_BAR);
+        Tile.EmptyTile.add(TileColours.EMPTY_TILE_SIDE + "        " + TileColours.EMPTY_TILE_SIDE);
+        Tile.EmptyTile.add(TileColours.EMPTY_TILE_SIDE + "        " + TileColours.EMPTY_TILE_SIDE);
+        Tile.EmptyTile.add(TileColours.EMPTY_TILE_BAR);
+
+
+        for (int i = 0; i < 100; i++) {
+            this.myTiles.add(Tile.EmptyTile);
+        }
+        placeStarterTile();
 
     }
 
-        public void fillBoard(){
-            Tile a = Tile.generateTile();
-            Tile b = Tile.generateTile();
-            Tile c = Tile.generateTile();
+    public void showBoard(int length) {
+        int start, end;
+        start = length;
+        end = length + 10;
 
-            board[MAX_BOARD / 2][MAX_BOARD / 2] = a;
-            board[MAX_BOARD / 2 - 1][MAX_BOARD / 2 + 1] = b;
-            board[MAX_BOARD / 2][MAX_BOARD / 2 + 1] = c;
 
-            for(int i = 0;i < 4;i++){
-                for(int j = 0;j < MAX_BOARD*MAX_BOARD;j++){
-                    Tile.allTiles.get(j).set(i, TileColours.EMPTY_TILE_SIDE);
+        for (int i = 0; i < 4; i++) {
+
+            System.out.print("\t\t\t\t\t\t");
+
+            for (int j = start; j < end; j++) {
+                System.out.print(this.myTiles.get(j).get(i));
+                if (j == end - 1 && i == 2) {
+                    System.out.print(" " + counter);
+                    counter++;
                 }
             }
-//            Tile.allTiles.get()
+
+            System.out.println();
+
         }
-        public void showBoard(int check){
-        int line = 4;
-        int start = 0;
-        int counter = 0;
-            if(check > 1){
-                start = line;
-                line *= check;
-
-            }
-            if(check == 2){
-                for(int i = 0; i < 4;i++){
-                    System.out.print("\t ");
-                    for(int j = start; j < line ;j++){
-                            System.out.print((Tile.allTiles.get(j).get(i)));
-                            counter++;
-                    }
-                    System.out.println();
-                }
-
-                    showBoard(1);
-            }
-            if(check == 1) {
-                for (int i = 0; i < 4; i++) {
-                    for (int j = start; j < line; j++) {
-                            System.out.print((Tile.allTiles.get(j).get(i)));
-                            counter++;
-                    }
-                    System.out.println();
-                }
-
-
-                    showBoard(2);
-                }
-            }
+        if (end != 100) {
+            showBoard(length + 10);
+        } else {
+            System.out.print("\t\t\t\t\t\t\t" + " 0" + "\t\t\t  1\t\t\t 2\t\t\t 3\t\t\t 4\t\t\t 5\t\t\t 6\t\t\t 7\t\t\t 8\t\t\t 9\n");
+            System.out.println("\t\t\t\t\t\t\tHere is your current board!\n\n");
+            counter = 0;
         }
 
+    }
+}
