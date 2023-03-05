58976cac1620d055ceedf787a3fee47b13b7dd2e
ethanflyn <102313981+ethanflyn@users.noreply.github.com>
2023-03-05 11:11:11 +0000

    Update Token.java

--- Token.java
+++ Token.java
@@ -4,20 +4,35 @@ import java.util.Collections;
 public class Token {
 
     public static ArrayList<String> wildlifeTokens = new ArrayList<>();
+    public static ArrayList<String> presentTokens = new ArrayList<>();
     public static void generateTokens() {
-        String F = "Fox";
-        String B = "Bear";
-        String H = "Hawk";
-        String E = "Elk";
-        String S = "Salmon";
+        String Fox = "F";
+        String Bear = "B";
+        String Hawk = "H";
+        String Elk = "E";
+        String Salmon = "S";
 
         for (int i = 0; i < 20; i++) {
-            wildlifeTokens.add(F);
-            wildlifeTokens.add(B);
-            wildlifeTokens.add(H);
-            wildlifeTokens.add(E);
-            wildlifeTokens.add(S);
+            wildlifeTokens.add(Fox);
+            wildlifeTokens.add(Bear);
+            wildlifeTokens.add(Hawk);
+            wildlifeTokens.add(Elk);
+            wildlifeTokens.add(Salmon);
         }
+    }
+
+    public static void playableTokens() {
         Collections.shuffle(wildlifeTokens);
+        int m = wildlifeTokens.size()-1;
+        for (int i = 0; i < 4; i++) {
+            presentTokens.add(wildlifeTokens.get(m));
+            wildlifeTokens.remove(m--);
+        }
+        for (int i = 0; i < 4; i++) {
+            System.out.print("     " + Token.presentTokens.get(i) + "\t\t\t");
+        }
+        System.out.println();
+        Culling.cullRequired();
+
     }
 }
