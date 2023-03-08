6aef0a8202513b8be66e9fa34b9ecc4bca2389bf
daramacken <102314081+daramacken@users.noreply.github.com>
2023-03-08 14:21:56 +0000

    Update Board.java

--- Board.java
+++ Board.java
@@ -1,5 +1,6 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 
@@ -9,21 +10,22 @@ public class Board {
     private static int counter = 0;
 
     ArrayList<ArrayList<String>> myTiles = new ArrayList<>();
+    ArrayList<String> chosenTile = new ArrayList<>();
 
     public Board() {
 
     }
 
 
-    public void placeTile() {
+    public void placeTile(ArrayList<String> tile) {
 
-        System.out.println("\t\t\t\t\t\t\tTILE PLACEMENT\n\t\t\t\t\t\t\tPlease enter the X cordinate of your desired tile\n");
+        System.out.println("TILE PLACEMENT\nPlease enter the X cordinate of your desired tile\n");
         Scanner a = new Scanner(System.in);
         String input = a.nextLine();
         int x = Integer.parseInt(input);
         if (x < 0 || x > 9) {
             System.out.println("\t\t\t\t\t\t\tInvalid input!!!!");
-            placeTile();
+            placeTile(tile);
         }
         System.out.println("\t\t\t\t\t\t\tPlease enter the Y cordinate of your desired tile");
         Scanner b = new Scanner(System.in);
@@ -31,26 +33,71 @@ public class Board {
         int y = Integer.parseInt(input2);
         if (y < 0 || y > 9) {
             System.out.println("\t\t\t\t\t\t\tInvalid input!!!!");
-            placeTile();
+            placeTile(tile);
         }
 
-        input += input2;
-        int position = Integer.parseInt(input);
+        input2 += input;
+        int position = Integer.parseInt(input2);
 
         if (this.myTiles.get(position + 1) == Tile.EmptyTile && this.myTiles.get(position - 1) == Tile.EmptyTile && this.myTiles.get(position + 10) == Tile.EmptyTile && this.myTiles.get(position - 10) == Tile.EmptyTile) {
             System.out.println("\t\t\t\t\t\t\tThere is no adjacent Tiless!!!!");
-            placeTile();
+            placeTile(tile);
         }
         if (this.myTiles.get(position) != Tile.EmptyTile) {
             System.out.println("\t\t\t\t\t\t\tThere is already a tile placed here!!!!");
-            placeTile();
+            placeTile(tile);
         }
 
-        this.myTiles.set(position, Tile.generateTile().TileDisplay());
+        this.myTiles.set(position, tile);
 
         showBoard(0);
         System.out.println("\n\n");
         GameIntro.NextTurn();
+    }
+
+    public void chooseTile(){
+        System.out.println("Please enter a number between 1-4 to choose which tile you would like to place!");
+        Scanner a = new Scanner(System.in);
+        int input = a.nextInt();
+        if(input > 4 || input < 0){
+            System.out.println("Invalid input");
+            chooseTile();
+        }
+         chosenTile = Tile.presentTiles.get(input - 1);
+
+        rotateTile(chosenTile);
+    }
+    public void rotateTile(ArrayList<String> tile){
+
+        System.out.println("Would you like to rotate your tile?\nEnter 'yes' or 'no'");
+        Scanner a = new Scanner(System.in);
+        String input = a.nextLine();
+    if(input.equals("no") ){
+        placeTile(tile);
+    }
+    else if(input.equals("yes")){
+        String habitat1 = tile.get(0);
+        String habitat2 = tile.get(3);
+        String bar1 = tile.get(1);
+        String bar2 = tile.get(2);
+        tile.set(0, habitat2);
+        tile.set(3,habitat1);
+        tile.set(1,bar2);
+        tile.set(2,bar1);
+        for(int i = 0;i < 4; i++){
+            System.out.println(tile.get(i));
+        }
+
+        placeTile(tile);
+    }
+    else{
+        System.out.println("Invalid input");
+        rotateTile(tile);
+    }
+
+
+
+
     }
 
     public void placeStarterTile() {
