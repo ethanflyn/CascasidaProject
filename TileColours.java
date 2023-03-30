9cba381b38cdcf4e5cc55663593326db96a0a476
ethanflyn <102313981+ethanflyn@users.noreply.github.com>
2023-03-30 15:04:59 +0100

    Update TileColours.java

--- TileColours.java
+++ TileColours.java
@@ -1,18 +1,17 @@
 
 public class TileColours {
-    public static final String PRAIRIE_BAR = "\u001B[43m" + "\t\t\t" + "\u001B[0m";
+    // Top/Bottom bars and left/right sides of Tile display using ANSI colour codes
+    public static final String PRAIRIE_BAR = "\u001B[43m" + "           " + "\u001B[0m";
     public static final String PRAIRIE_SIDE = "\u001B[43m" + "  " + "\u001B[0m";
-    public static final String FOREST_BAR = "\u001B[42m" + "\t\t\t" + "\u001B[0m";
+    public static final String FOREST_BAR = "\u001B[42m" + "           " + "\u001B[0m";
     public static final String FOREST_SIDE = "\u001B[42m" + "  " + "\u001B[0m";
-    public static final String WETLAND_BAR = "\u001B[102m" + "\t\t\t" + "\u001B[0m";
+    public static final String WETLAND_BAR = "\u001B[102m" + "           " + "\u001B[0m";
     public static final String WETLAND_SIDE = "\u001B[102m" + "  " + "\u001B[0m";
-    public static final String RIVER_BAR = "\u001B[44m" + "\t\t\t" + "\u001B[0m";
+    public static final String RIVER_BAR = "\u001B[44m" + "           " + "\u001B[0m";
     public static final String RIVER_SIDE = "\u001B[44m" + "  " + "\u001B[0m";
-    public static final String MOUNTAIN_BAR = "\u001B[100m" + "\t\t\t" + "\u001B[0m";
+    public static final String MOUNTAIN_BAR = "\u001B[100m" + "           " + "\u001B[0m";
     public static final String MOUNTAIN_SIDE = "\u001B[100m" + "  " + "\u001B[0m";
-    public static final String EMPTY_TILE_BAR= "\u001B[40m" + "\t\t\t" + "\u001B[0m";
-    public static final String EMPTY_TILE_SIDE ="\u001B[40m"+ "  " + "\u001B[0m";
-
-   
+    public static final String EMPTY_TILE_BAR= "\u001B[40m" + "           " + "\u001B[0m";
+    public static final String EMPTY_TILE_SIDE= "\u001B[40m" + "  " + "\u001B[0m";
 
 }
