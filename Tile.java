bbd414a8c3d510dc7f016c7b62d00a01a33ed09e
ethanflyn <102313981+ethanflyn@users.noreply.github.com>
2023-04-13 15:40:53 +0100

    Update Tile.java

--- Tile.java
+++ Tile.java
@@ -150,12 +150,12 @@ public class Tile {
         ArrayList<String> theTile = new ArrayList<>();
 
         theTile.add(habitatTopBar);
-        theTile.add(habitatLeftSide + "  " + this.tileWildlife.get(0) + "  " +
+        theTile.add(habitatLeftSide + "\t" + this.tileWildlife.get(0) + "\t" +
                 (this.tileWildlife.size() > 1 ? this.tileWildlife.get(1): " ") + " " + habitatRightSide);
         if (this.tileHabitats.size() == 1)
-            theTile.add(habitatLeftSide + (this.tileWildlife.size()>2 ? this.tileWildlife.get(2): " ") + "    K " + habitatRightSide);
+            theTile.add(habitatLeftSide + (this.tileWildlife.size()>2 ? this.tileWildlife.get(2): " ") + "\t\tK " + habitatRightSide);
         else {
-            theTile.add(habitatLeftSide + "  " + (this.tileWildlife.size()>2 ? this.tileWildlife.get(2): " ") + "    " + habitatRightSide);
+            theTile.add(habitatLeftSide + "\t" + (this.tileWildlife.size()>2 ? this.tileWildlife.get(2): " ") + "\t  " + habitatRightSide);
         }
         theTile.add(habitatBottomBar);
 
@@ -163,8 +163,8 @@ public class Tile {
 
     }
     
-    public static void tileBag(int numUsers) {
-        switch (numUsers) {
+    public static void tileBag() {
+        switch (GameIntro.users.size()) {
             case 4:
                 for (int i = 0; i < 83; i++) {
                     Tile temp = generateTile();
@@ -178,7 +178,7 @@ public class Tile {
                 }
                 break;
             case 2:
-                for (int i = 0; i < 8; i++) {
+                for (int i = 0; i < 43; i++) {
                     Tile temp = generateTile();
                     allTiles.add(temp.TileDisplay());
                 }
@@ -200,7 +200,7 @@ public class Tile {
             System.out.print("\t\t\t\t\t\t");
             for (int j = 0; j < 4; j++) {
                 System.out.print(presentTiles.get(j).get(i));
-                System.out.print("			");
+                System.out.print("\t");
             }
             System.out.println();
         }
@@ -218,5 +218,16 @@ public class Tile {
         Collections.shuffle(allTiles);
     }
 
+    @Override
+    public String toString() {
+        return "Tile{}";
+    }
 
+    public static void main(String[] args) {
+        habitatArray();
+        wildlifeArray();
+        Token.generateTokens();
+        tileBag();
+        playableTiles();
+    }
 }
