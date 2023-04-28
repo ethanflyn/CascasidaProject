6f38618a02f3cc3987e3b02ace0db1d265e7cc8d
daramacken <102314081+daramacken@users.noreply.github.com>
2023-04-28 09:40:17 +0100

    Update Bot.java

--- Bot.java
+++ Bot.java
@@ -54,6 +54,55 @@ public class Bot {
             throw new IllegalArgumentException("No token can be placed");
         }
     }
+    
+    
+
+    public static char placeTileXCord(ArrayList<ArrayList<String>> boardOfTiles,  ArrayList<String> tile){
+        placeTile(boardOfTiles, tile);
+        if (tileCord < 10) return '0';
+        else{
+           char ans = Integer.toString(tileCord).charAt(0);
+           System.out.println(ans);
+           return ans;
+       }
+    }
+    public static char placeTileYCord(ArrayList<ArrayList<String>> boardOfTiles, ArrayList<String> tile){
+        char ans = Integer.toString(tileCord).charAt(1);
+
+        return ans;
+    }
+
+
+    public static void placeTile(ArrayList<ArrayList<String>> boardOfTiles, ArrayList<String> tile){
+
+        ArrayList<Integer> tileIndexes = new ArrayList<>();
+
+
+
+
+            for(int j = 0;j < boardOfTiles.size();j++){
+                if(!boardOfTiles.get(j).get(0).equals(TileColours.EMPTY_TILE_BAR)) {
+                    tileIndexes.add(j);
+                }
+            }
+
+                Collections.shuffle(tileIndexes);
+        System.out.println(tileIndexes.get(0));
+            if(boardOfTiles.get(tileIndexes.get(0) + 1).get(0).equals(TileColours.EMPTY_TILE_BAR) && tileIndexes.get(0) % 10 != 9){
+                tileCord =  tileIndexes.get(0) + 1;
+            }
+            else if(boardOfTiles.get(tileIndexes.get(0) - 1).get(0).equals(TileColours.EMPTY_TILE_BAR) && tileIndexes.get(0) % 10 != 0){
+                tileCord = tileIndexes.get(0) - 1;
+            }
+            else if(boardOfTiles.get(tileIndexes.get(0) + 10).get(0).equals(TileColours.EMPTY_TILE_BAR) && tileIndexes.get(0) < 90){
+                tileCord = tileIndexes.get(0) + 10;
+            }
+            else if(boardOfTiles.get(tileIndexes.get(0) - 10).get(0).equals(TileColours.EMPTY_TILE_BAR) && tileIndexes.get(0) > 10){
+                tileCord = tileIndexes.get(0) - 10;
+            }
+
+    }
+    
 
     public static int botScoring(ArrayList<ArrayList<String>> tiles, int natureTokens) {
         int score = 0;
